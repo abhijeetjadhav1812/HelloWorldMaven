8d4ceee248c1ff75ddbbf1ea21672d224a5714bf
Abhijeet R Jadhav <49363614+abhijeetjadhav1812@users.noreply.github.com>
2021-04-12 15:17:38 +0530

    try # 2

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Welcome to Maven integration" );
-         System.out.println( "TRY Number 1" );
+         System.out.println( "TRY Number 2" );
     }
 }
