febb97cc98e9fe6fb78ca3abf7bb11b325766456
Abhijeet R Jadhav <49363614+abhijeetjadhav1812@users.noreply.github.com>
2021-04-21 19:53:47 +0530

    Change to verify poll SCM
    TO checking if POLL SCM works if a change is done in the java-code

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World! Welcome to Maven integration" );
          System.out.println( "TRY Number 2" );
+        System.out.println( "To check if this triggers poll SCM" );
+         System.out.println( "TRY Number 1" );
     }
 }
